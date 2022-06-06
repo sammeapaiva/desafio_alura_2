123b46057f1b68b27ea783b3e568617e0be50ec2
sammeapaiva <sammeapaiva@gmail.com>
2022-06-05 20:13:55 -0400

    05.06-2

--- js/usando_storage.js
+++ js/usando_storage.js
@@ -3,9 +3,10 @@ botaoDesistir.addEventListener('click',function(){
 	var palavras = JSON.parse(localStorage.getItem("palavras"));
 	const palavraAleatoria = palavras[Math.floor(Math.random() * palavras.length)];
 	console.log(palavraAleatoria);
-});
 
-var novo = document.createElement('p');
-novo.appendChild(palavraAleatoria);
-var teclas = document.querySelector('.teclas');
-teclas.appendChild(novo);
+	var novo = document.createElement('p');
+	novo.appendChild(palavraAleatoria);
+	var teclas = document.querySelector('.teclas');
+	teclas.appendChild(novo);
+
+});
